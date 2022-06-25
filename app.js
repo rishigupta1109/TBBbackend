e447964bbcda6d431f456d02f5236f5608d4593a
Rishi <grishi634@gmail.com>
2022-06-25 20:02:06 +0530

    cors error

--- app.js
+++ app.js
@@ -117,6 +117,7 @@ app.use((req, res, next) => {
     "Origin , X-Requested-With,Content-Type, Accept, Authorization"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  console.log("headers seted");
   next();
 });
 app.use("/api/books", bookRoutes);
