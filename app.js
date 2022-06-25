3a42d073b1fe5f40f410c47f65d63eae981dd590
Rishi <grishi634@gmail.com>
2022-06-25 19:54:36 +0530

    cors error

--- app.js
+++ app.js
@@ -106,6 +106,7 @@ app.use(bodyParser.json());
 // app.use(cors(corsOpts));
 // app.options("*", cors());
 app.use((req, res, next) => {
+  console.log("setting headers")
   res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
   res.setHeader(
     "Access-Control-Allow-Credentials",
