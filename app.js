8cd4733c2ece000bbeb2e8654eb096862a342c0c
Rishi <grishi634@gmail.com>
2022-06-26 14:38:20 +0530

    cors

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.use(bodyParser.json());
 // app.use(cors(corsOpts));
 // app.options("*", cors());
 app.use((req, res, next) => {
-  console.log("setting headers")
+  console.log("setting headers");
   res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
   res.setHeader(
     "Access-Control-Allow-Credentials",
